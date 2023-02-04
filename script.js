89262c9656fec5a682e6a44cd86247d752a136ee
oscar cervantes <oscarcr189@outlook.com>
2023-02-03 23:21:17 -0600

    actualizacion

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ window.onscroll =function(){
 //funcion que aplica la animacion de la barra de habilidades
 function efectoHabilidades(){
     var skills= document.getElementById("skills")
-    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    var distancia_skills = window.innerHeight-skills.getBoundingClientRect().top;
     if(distancia_skills >= 300){
         document.getElementById("html").classList.add("barra-progreso1")
         document.getElementById("js").classList.add("barra-progreso2")
